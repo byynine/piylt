12af0565cd24ec370f62b90271adece1d74ab254
byynine <nutitota@tutanota.com>
2026-01-31 21:56:10 +0400

    draw dot at mouse position on lmb down

--- src/main.c
+++ src/main.c
@@ -50,46 +50,41 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    bool done = false;
+    float xf;
+    float yf;
+    
+    bool mb_down = false;
+
+    bool run = true;
     SDL_Event event;
 
-    while (!done)
+    while (run)
     {
         while (SDL_PollEvent(&event))
         {
-            if (event.type == SDL_EVENT_QUIT) done = true;
+            if (event.type == SDL_EVENT_QUIT) run = false;
             if (event.type == SDL_EVENT_MOUSE_MOTION)
             {
-                float xf = event.motion.x / window_size[0];
-                float yf = event.motion.y / window_size[1];
-
-                for (int i = 0; i < terminal_window.ws_row * yf; i++)
-                {
-                    for (int j = 0; j < terminal_window.ws_col * xf; j++) putchar(' ');
-                    putchar('.');
-                    putchar('\n');
-                };
-                printf("xf: %.2f yf: %.2f\n", xf, yf);
-
-                fflush(stdout);
+                xf = event.motion.x / window_size[0];
+                yf = event.motion.y / window_size[1];
             }
-            if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN)
-            {
-                printf("MB1_DOWN\n");
-                fflush(stdout);
-            }
-            if (event.type == SDL_EVENT_MOUSE_BUTTON_UP)
-            {
-                printf("MB1_UP\n");
-                fflush(stdout);
-            }
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN) mb_down = true;
+            if (event.type == SDL_EVENT_MOUSE_BUTTON_UP) mb_down = false;
         }
 
-        printf("\033[2J\033[H");
+        if (mb_down)
+        {
+            for (int i = 0; i < terminal_window.ws_row * yf; i++)
+            {
+                for (int j = 0; j < terminal_window.ws_col * xf; j++) putchar(' ');
+                if ((i + 1) >= terminal_window.ws_row * yf) putchar('.');
+                putchar('\n');
+            };
 
+            printf("\033[2J\033[H");
+        }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
 
